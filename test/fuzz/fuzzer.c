5fd45ff24ba37edeee60d8b34be2c99537c1b15a
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-08-29 16:41:39 +0200

    Exit instead of aborting

--- test/fuzz/fuzzer.c
+++ test/fuzz/fuzzer.c
@@ -21,14 +21,14 @@ int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size) {
     int fd = shm_open(SHMPATH, O_RDWR, S_IRUSR | S_IWUSR);
     if(fd == -1) {
         perror("shm_open");
-        abort();
+        exit(1);
     }
 
     struct shmbuf *shmb = mmap(0, sizeof(*shmb), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     close(fd);
     if(shmb == MAP_FAILED) {
         perror("mmap");
-        abort();
+        exit(1);
     }
 
     sem_wait(&shmb->sem);
@@ -38,7 +38,7 @@ int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size) {
 
     if(sem_post(&shmb->sem) == -1) {
         perror("sem_post");
-        abort();
+        exit(1);
     }
 
     munmap(shmb, sizeof(*shmb));
